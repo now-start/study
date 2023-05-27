2a47fcb654df82794caedeee022824251186c570
moon <nowstart2691@gmail.com>
2023-05-28 02:50:04 +0900

    fix: swagger config 추가 및 버그 수정

--- src/main/java/org/nowstart/study/domain/vo/response/CommResponseVo.java
+++ src/main/java/org/nowstart/study/domain/vo/response/CommResponseVo.java
@@ -1,13 +1,11 @@
 package org.nowstart.study.domain.vo.response;
 
-import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
-@JsonRootName("result")
 public class CommResponseVo {
 
     @Builder.Default
--- src/main/java/org/nowstart/study/domain/vo/response/UserResponseVo.java
+++ src/main/java/org/nowstart/study/domain/vo/response/UserResponseVo.java
@@ -1,13 +1,15 @@
 package org.nowstart.study.domain.vo.response;
 
 import com.fasterxml.jackson.annotation.JsonRootName;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import lombok.Getter;
 import lombok.experimental.SuperBuilder;
 
 @Getter
 @SuperBuilder
-@JsonRootName("result")
 public class UserResponseVo extends CommResponseVo {
+
     String id;
     String password;
 }
