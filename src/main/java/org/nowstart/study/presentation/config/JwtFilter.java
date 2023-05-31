bb79b6e9e8ceadb11eae4514ef4b0e1e0120f7e4
moon <nowstart2691@lguplus.co.kr>
2023-05-31 16:11:46 +0900

    feat(JWT): Add JWT to SpringSecurityConfig

--- src/main/java/org/nowstart/study/presentation/config/JwtFilter.java
+++ src/main/java/org/nowstart/study/presentation/config/JwtFilter.java
@@ -6,20 +6,17 @@ import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.GenericFilterBean;
 
 @Component
+@RequiredArgsConstructor
 public class JwtFilter extends GenericFilterBean {
 
-    private JwtProvider provider;
-
-    public JwtFilter(JwtProvider provider) {
-        this.provider = provider;
-    }
+    private final JwtProvider provider;
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
