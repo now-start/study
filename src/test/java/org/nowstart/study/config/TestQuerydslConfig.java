6b150f5d670d6ebde12f0e417e8cc7cc4a01f76b
moon <nowstart2691@gmail.com>
2023-07-19 22:28:19 +0900

    test : 테스트 케이스 추가 및 gradle 변경

--- src/test/java/org/nowstart/study/config/TestQuerydslConfig.java
+++ src/test/java/org/nowstart/study/config/TestQuerydslConfig.java
@@ -5,9 +5,11 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @TestConfiguration
-class QuerydslConfigTest {
+public class TestQuerydslConfig {
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -16,5 +18,4 @@ class QuerydslConfigTest {
     public JPAQueryFactory jpaQueryFactory() {
         return new JPAQueryFactory(entityManager);
     }
-
 }
--- src/test/java/org/nowstart/study/repository/impl/BoardQueryDslRepositoryImplTest.java
+++ src/test/java/org/nowstart/study/repository/impl/BoardQueryDslRepositoryImplTest.java
@@ -1,26 +1,26 @@
 package org.nowstart.study.repository.impl;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
+import org.nowstart.study.config.TestQuerydslConfig;
 import org.nowstart.study.data.dto.board.BoardFindServiceDto;
 import org.nowstart.study.data.entity.BoardEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.List;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DataJpaTest
+@Import({TestQuerydslConfig.class})
 @ExtendWith(SpringExtension.class)
 class BoardQueryDslRepositoryImplTest {
 
-    @InjectMocks
-    BoardQueryDslRepositoryImpl boardQueryDslRepository;
-
     @Autowired
-    JPAQueryFactory jpaQueryFactory;
+    BoardQueryDslRepositoryImpl boardQueryDslRepository;
 
     @Test
     void findAll() {
@@ -30,8 +30,9 @@ class BoardQueryDslRepositoryImplTest {
 
         //when
         List<BoardEntity> result = boardQueryDslRepository.findAll(boardFindServiceDto);
+        System.out.println(result);
 
         //then
-
+        assertThat(result).isEmpty();
     }
 }
